a2fb6f85e9226fbf93d82e9e363d3faf4d318e2b
maha-anugu09 <38092955+maha-anugu09@users.noreply.github.com>
2018-04-06 08:45:29 -0400

    Updated

--- NGINXInstall.ps1
+++ NGINXInstall.ps1
@@ -1,52 +1,40 @@
 
-sudo apt-get install nginx
-
-# sudo vim /etc/nginx/nginx.conf
-user  nginx;
-worker_processes  2;
-
-error_log  /var/log/nginx/error.log warn;
-pid        /var/run/nginx.pid;
-
-
-events {
-    worker_connections  1024;
-}
-
-
-http {
-    include       /etc/nginx/mime.types;
-    default_type  application/octet-stream;
-
-    access_log  /var/log/nginx/access.log  main;
-
-
-    include /etc/nginx/sites-available/*.conf;
-	http {
-    include       mime.types;
-    default_type  application/octet-stream;
-    sendfile        on;
-    keepalive_timeout  65;
-
-    server {
-        listen       80;
-
-		-- Change the localhost to the servername
-        server_name  localhost;
-        root  /nginx/html/organizr;
-
-        index index.php index.html index.htm;       
-	    error_page 400 401 403 404 405 408 500 502 503 504  /error.php?error=$status;
-        location ~ \.php$ {
-            fastcgi_pass   127.0.0.1:9000;
-            fastcgi_index  index.php;
-            fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
-            include        fastcgi_params;
+#cloud-config
+package_upgrade: true
+packages:
+  - nginx
+  - nodejs
+  - npm
+write_files:
+  - owner: www-data:www-data
+  - path: /etc/nginx/sites-available/default
+    content: |
+      server {
+        listen 80;
+        location / {
+          proxy_pass http://localhost:3000;
+          proxy_http_version 1.1;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection keep-alive;
+          proxy_set_header Host $host;
+          proxy_cache_bypass $http_upgrade;
         }
-
-    }
-
-}
-
-
-
+      }
+  - owner: azureuser:azureuser
+  - path: /home/azureuser/myapp/index.js
+    content: |
+      var express = require('express')
+      var app = express()
+      var os = require('os');
+      app.get('/', function (req, res) {
+        res.send('app from host ' + os.hostname() + '!')
+      })
+      app.listen(3000, function () {
+        console.log('app listening on port 3000!')
+      })
+runcmd:
+  - service nginx restart
+  - cd "/home/azureuser/myapp"
+  - npm init
+  - npm install express -y
+  - nodejs index.js
