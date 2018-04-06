e604c2017c167f3ae6fa29659b5c0f5cc3828407
maha-anugu09 <38092955+maha-anugu09@users.noreply.github.com>
2018-04-06 08:26:54 -0400

    Updated

--- NGINXInstall.ps1
+++ NGINXInstall.ps1
@@ -1,3 +1,6 @@
+
+sudo apt-get install nginx
+
 # sudo vim /etc/nginx/nginx.conf
 user  nginx;
 worker_processes  2;
@@ -15,12 +18,35 @@ http {
     include       /etc/nginx/mime.types;
     default_type  application/octet-stream;
 
-    log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
-                      '$status $body_bytes_sent "$http_referer" '
-                      '"$http_user_agent" "$http_x_forwarded_for"';
-
     access_log  /var/log/nginx/access.log  main;
 
 
     include /etc/nginx/sites-available/*.conf;
+	http {
+    include       mime.types;
+    default_type  application/octet-stream;
+    sendfile        on;
+    keepalive_timeout  65;
+
+    server {
+        listen       80;
+
+		-- Change the localhost to the servername
+        server_name  localhost;
+        root  /nginx/html/organizr;
+
+        index index.php index.html index.htm;       
+	    error_page 400 401 403 404 405 408 500 502 503 504  /error.php?error=$status;
+        location ~ \.php$ {
+            fastcgi_pass   127.0.0.1:9000;
+            fastcgi_index  index.php;
+            fastcgi_param  SCRIPT_FILENAME $document_root$fastcgi_script_name;
+            include        fastcgi_params;
+        }
+
+    }
+
 }
+
+
+
